ed3850054c723ed6ad9c04f8238e640d471e6f9f
Meshack <mbuvi.meshack99@gmail.com>
2022-10-27 00:35:59 +0300

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+attr_accessor :word
+
+def initialize(word)
+    @word = word
+end
+
+def match(array)
+   sorted_word = @word.chars.sort.join
+   array.select{ |w| w.chars.sort.join == sorted_word}
+end
+end
